4a6013fb61eac914d51947a749a8fa954b3a448f
Kyle Cornelissen <kyle.cornelissen@gmail.com>
2019-03-15 08:28:59 -0600

    initial commit

---
+++ lib/turn.rb
@@ -0,0 +1,35 @@
+
+
+
+class Turn
+  attr_reader :guess,
+              :card,
+              :answer
+
+
+  def initialize(guess_arg, card_arg)
+    @guess = guess_arg
+    @card = card_arg
+    correct = true
+  end
+
+  def correct?
+    if guess == card.answer
+
+      @correct = true
+    else
+      @correct = false
+    end
+
+  end
+
+  def feedback
+    if @correct == true
+      "Correct!"
+    else
+      "Incorrect."
+    end
+
+  end
+
+end
